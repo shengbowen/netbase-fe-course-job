1238235e416759263337252f06607d037eac8be1
shengbowen <shengbowen123@gmail.com>
2016-06-15 22:37:30 +0800

    创建sass文件，编写头部样式

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,44 @@
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer');
+	browerSync = require('browser-sync').create(),
+	reload = browerSync.reload;
 
 
-gulp.task('default', function(){
-	console.log('hello gulp!');
-});
+gulp.task('default', ['serve']);
 
-gulp.task('task1', function(){
-	console.log('task1');
-});
+// gulp.task('task1', function(){
+// 	console.log('task1');
+// });
 
-gulp.task('style', function(){
-	return gulp.src('src/main.scss')
-	           .pipe(sass({'style': 'expanded'}))
+// gulp.task('style', function(){
+// 	return gulp.src('src/main.scss')
+// 	           .pipe(sass({'style': 'expanded'}))
 	           
-	           .pipe(gulp.dest('dist/css'));
+// 	           .pipe(gulp.dest('dist/css'));
+// });
+
+// gulp.task('to', function () {
+// 	return sass('css/main.scss', {'style': 'expanded'})
+// 				.pipe(autoprefixer('last 2 version', 'safari5', 'ie8', 'ie9', 'opera12.1', 'ios6', 'android4'))
+// 				.pipe(gulp.dest('dist/css'));
+// });
+
+gulp.task('browser-sync', function(){
+	browerSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
 });
 
-gulp.task('to', function () {
-	return sass('css/main.scss', {'style': 'expanded'})
-				.pipe(autoprefixer('last 2 version', 'safari5', 'ie8', 'ie9', 'opera12.1', 'ios6', 'android4'))
-				.pipe(gulp.dest('dist/css'));
-})
+gulp.task('serve', ['sass'],function(){
+	browerSync.init({server: "./"});
+	gulp.watch("views/index.html").on('change', reload);
+	gulp.watch("src/css/*.scss", ['sass']);
+});
+
+gulp.task('sass', function(){
+	return sass('src/css/style.scss', {'style': 'expanded'})
+				.pipe(gulp.dest('dist/css/'))
+				.pipe(reload({stream: true}));
+});
